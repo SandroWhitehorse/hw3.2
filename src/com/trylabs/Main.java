027d309f04627a1b2de3962bb986117eeb31f2d6
Sandro Whitehorse <sg3802307@gmail.com>
2017-06-12 17:01:36 +0300

    Initial commit

---
+++ src/com/trylabs/Main.java
@@ -0,0 +1,11 @@
+package com.trylabs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (double x=0; x<=5; x=x+0.5)
+    {
+    System.out.println(3*x*x-5);
+    }
+    }
+}
